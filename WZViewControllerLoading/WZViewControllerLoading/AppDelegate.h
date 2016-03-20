230217812c579d1be5878751a3a03b120cd49347
WilliamMaybe <271138178@qq.com>
2016-03-21 01:42:40 +0800

    Init

---
+++ WZViewControllerLoading/WZViewControllerLoading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZViewControllerLoading
+//
+//  Created by WilliamZhang on 16/3/21.
+//  Copyright © 2016年 WilliamZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
